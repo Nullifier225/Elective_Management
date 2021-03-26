61fa6fe386addabf04267b76f92b718d544eb412
monisha0130 <monishaselvaraj2001@gmail.com>
2021-03-26 18:55:04 +0530

    adminstudentlist
    done

--- elective_management/src/component/adminstudentlist.js
+++ elective_management/src/component/adminstudentlist.js
@@ -10,34 +10,49 @@ export default class adminstudentlist extends Component {
         
     axios.get('http://localhost:3001/api/getlist1').then(response=>{
     
-    
+
     var details = response.data;
     var element;
     alert(details)
-    for(let i=0;i<response.data.length;i++){
+    var i=0
     let x1=JSON.parse(JSON.stringify(details[i].content[0]))
     let x2=JSON.parse(JSON.stringify(details[i].content[1]))
     let x3=JSON.parse(JSON.stringify(details[i].content[2]))
     let x4=JSON.parse(JSON.stringify(details[i].content[3]))
     let x5=JSON.parse(JSON.stringify(details[i].content[4]))
     alert(x1)
-      var li=ReactDOM.render(li,document.createElement("li"));
-      var text=ReactDOM.render(text,document.createTextNode(x1));
-      ReactDOM.render(li.appendChild(text));
-      ReactDOM.render(document.getElementById("data").appendChild(li));
+      
+    element=<table>
+    <tr>
+    <th>Name</th>
+    <th>Roll.no</th>
+    <th>section</th>
+    <th>Preference number</th>
+    <th>Elective</th>
+    </tr>
+    {details.map((item) => (<tr>
+      
+      <td>{JSON.parse(JSON.stringify(item.content[0]))}</td>
+      <td>{JSON.parse(JSON.stringify(item.content[1]))}</td>
+      <td>{JSON.parse(JSON.stringify(item.content[2]))}</td>
+      <td>{JSON.parse(JSON.stringify(item.content[3]))}</td>
+      <td>{JSON.parse(JSON.stringify(item.content[4]))}</td>
+      
+      </tr>
+
+    ))}
+    </table>
+
+  
+
+
+  /*element=<div class="table-data">
+  
     
-    /*element=<ul>
-  <li>name:{x1}</li>   
-  <li>rno:{x2}</li>
-  <li>section:{x3}</li>
-  <li>preference:{x4}</li>
-  <li>elective:{x5}</li>
-  </ul>*/
-    
-    }
-  alert(JSON.stringify(element))
-  //ReactDOM.render(element, document.getElementById('data'));
-    
+    */
+  //alert(JSON.stringify(element))
+  ReactDOM.render(element, document.getElementById('data'));
+
 
    
     
@@ -60,7 +75,7 @@ export default class adminstudentlist extends Component {
             
             <br></br>
             
-           
+                       
             <div id="data"></div>
             </div>
         );
