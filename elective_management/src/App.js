1ed426dc25672ad18177a431ce2db68cf574e76c
Nullifier225 <55820583+Nullifier225@users.noreply.github.com>
2021-06-08 12:13:00 +0530

    desperate attempts1

--- elective_management/src/App.js
+++ elective_management/src/App.js
@@ -35,6 +35,7 @@ class App extends Component {
   }
   render() {
     return (
+     <div> <h1> Shyamama </h1> 
 <BrowserRouter>
         
             <Switch>
@@ -62,5 +63,6 @@ class App extends Component {
             </Switch>
             
 </BrowserRouter>
+</div>
     )}}
 export default App;
