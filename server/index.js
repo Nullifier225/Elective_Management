365b8c7d2718212bbac957d91b9b613dd67dc63c
vsvt2000 <viswajithvishnusai@gmail.com>
2021-06-11 17:34:33 +0530

    udpates

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 process.on('uncaughtException', function (error) {
